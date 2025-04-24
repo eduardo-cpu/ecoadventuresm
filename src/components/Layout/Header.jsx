adaa4c3dd7af0ddb70836b8dcf5a2320b704910f
Eduardo <eduardosantossm@hotmail.com>
2025-04-23 21:56:25 -0300

    correção logo

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
                 <div className="flex items-center justify-between h-16 px-4">
                     <Link to="/" className="flex-shrink-0">
                         <img 
-                            src="./image.png" 
+                            src="/image.png" 
                             alt="EcoAdventureLogo" 
                             className="h-10"
                         />
